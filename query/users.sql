d308eb79dd44c72977dc0daab931932ca4e50bbe
Hibban Nurcholis <hibban.nurcholis@students.amikom.ac.id>
2023-05-30 05:23:06 +0700

    add: File user.sql Menambahkan file query insert tabel users berdasarkan data dari https://reqres.in/api/users

---
+++ query/users.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (id, first_name, last_name, email, avatar)
+VALUES
+(1, 'George', 'Bluth', 'george.bluth@reqres.in', 'https://reqres.in/img/faces/1-image.jpg'),
+(2, 'Janet', 'Weaver', 'janet.weaver@reqres.in', 'https://reqres.in/img/faces/2-image.jpg'),
+(3, 'Emma', 'Wong', 'emma.wong@reqres.in', 'https://reqres.in/img/faces/3-image.jpg'),
+(4, 'Eve', 'Holt', 'eve.holt@reqres.in', 'https://reqres.in/img/faces/4-image.jpg'),
+(5, 'Charles', 'Morris', 'charles.morris@reqres.in', 'https://reqres.in/img/faces/5-image.jpg'),
+(6, 'Tracey', 'Ramos', 'tracey.ramos@reqres.in', 'https://reqres.in/img/faces/6-image.jpg'),
+(7, 'Michael', 'Lawson', 'michael.lawson@reqres.in', 'https://reqres.in/img/faces/7-image.jpg'),
+(8, 'Lindsay', 'Ferguson', 'lindsay.ferguson@reqres.in', 'https://reqres.in/img/faces/8-image.jpg'),
+(9, 'Tobias', 'Funke', 'tobias.funke@reqres.in', 'https://reqres.in/img/faces/9-image.jpg'),
+(10, 'Byron', 'Fields', 'byron.fields@reqres.in', 'https://reqres.in/img/faces/10-image.jpg'),
+(11, 'George', 'Edwards', 'george.edwards@reqres.in', 'https://reqres.in/img/faces/11-image.jpg'),
+(12, 'Rachel', 'Howell', 'rachel.howell@reqres.in', 'https://reqres.in/img/faces/12-image.jpg');
